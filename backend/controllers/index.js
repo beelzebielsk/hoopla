9696caba3deffe757f574fd1592e4eb5d85535fe
adam <ibrahimadam193@gmail.com>
2017-11-09 18:54:00 -0500

    Fix access-control-cross-origin problem in backend.
    Changes to be committed:
          modified:   backend/controllers/index.js

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
