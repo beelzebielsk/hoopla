a3925b371717558e591ee2e1ad8810f6d53631e2
Chris Panican <chrispanican@gmail.com>
2017-12-06 13:28:46 -0500

    Add registration page

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Input, Button } from 'react-materialize';
+import '../styles/register.css';
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="registration-form">
+                <h4>Register</h4>
+                <form className="form-wrap register-table">
+                    <label>Username</label>
+                    <Input type="text" id="register-user"/>
+                    <label>E-mail</label>
+                    <Input type="text" id="email-user"/>
+                    <label>Password</label>
+                    <Input type="password" id="password-user"/>
+
+                    <form className="button-wrap">
+                        <Button>Submit</Button>
+                    </form>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Register;
